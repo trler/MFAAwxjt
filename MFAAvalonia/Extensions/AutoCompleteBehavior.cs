56bf5229fc71780d51e9e9274eae28ba30e11197
SweetSmellFox <1254371679@qq.com>
2025-07-06 00:25:41 +0800

    perf: 优化自动补全框的输入和下拉框弹出逻辑

--- MFAAvalonia/Extensions/AutoCompleteBehavior.cs
+++ MFAAvalonia/Extensions/AutoCompleteBehavior.cs
@@ -1,7 +1,10 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia.Collections;
+using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Xaml.Interactivity;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace MFAAvalonia.Extensions;
@@ -12,6 +15,10 @@ internal class AutoCompleteBehavior : Behavior<AutoCompleteBox>
     {
     }
 
+    private bool _justSelectedItem = false;
+    // 反射获取_view字段
+    private FieldInfo? _viewField;
+
     protected override void OnAttached()
     {
         if (AssociatedObject is not null)
@@ -19,6 +26,11 @@ internal class AutoCompleteBehavior : Behavior<AutoCompleteBox>
             AssociatedObject.KeyUp += OnKeyUp;
             AssociatedObject.GotFocus += OnGotFocus;
             AssociatedObject.DropDownOpening += DropDownOpening;
+            AssociatedObject.SelectionChanged += OnSelectionChanged;
+
+            // 获取_view字段（非公共）
+            _viewField = typeof(AutoCompleteBox)
+                .GetField("_view", BindingFlags.Instance | BindingFlags.NonPublic);
         }
 
         base.OnAttached();
@@ -28,23 +40,36 @@ internal class AutoCompleteBehavior : Behavior<AutoCompleteBox>
     {
         if (AssociatedObject is not null)
         {
-            AssociatedObject.KeyUp += OnKeyUp;
-            AssociatedObject.GotFocus += OnGotFocus;
-            AssociatedObject.DropDownOpening += DropDownOpening;
+            AssociatedObject.KeyUp -= OnKeyUp;
+            AssociatedObject.GotFocus -= OnGotFocus;
+            AssociatedObject.DropDownOpening -= DropDownOpening;
+            AssociatedObject.SelectionChanged -= OnSelectionChanged;
         }
         base.OnDetaching();
     }
-    
+
     private void OnKeyUp(object? sender, KeyEventArgs e)
     {
-        ShowDropdown();
+        if (ShouldShowDropdown(sender))
+        {
+            ShowDropdown();
+        }
     }
 
     private void OnGotFocus(object? sender, GotFocusEventArgs e)
     {
-        ShowDropdown();
+        if (!_justSelectedItem && sender is AutoCompleteBox autoCompleteBox && string.IsNullOrEmpty(autoCompleteBox.Text) && autoCompleteBox.ItemsSource != null && autoCompleteBox.ItemsSource.Cast<object>().Any())
+        {
+            ShowDropdown();
+        }
+        _justSelectedItem = false;
     }
-    
+
+    private void OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        _justSelectedItem = true;
+    }
+
     private void DropDownOpening(object? sender, System.ComponentModel.CancelEventArgs e)
     {
         var prop = AssociatedObject?.GetType().GetProperty("TextBox", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
@@ -60,12 +85,18 @@ internal class AutoCompleteBehavior : Behavior<AutoCompleteBox>
     {
         if (AssociatedObject is not null && !AssociatedObject.IsDropDownOpen)
         {
-            typeof(AutoCompleteBox).GetMethod("PopulateDropDown", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.Invoke(AssociatedObject, new object[] { AssociatedObject, EventArgs.Empty });
-            typeof(AutoCompleteBox).GetMethod("OpeningDropDown", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.Invoke(AssociatedObject, new object[] { false });
+            typeof(AutoCompleteBox).GetMethod("PopulateDropDown", BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.Invoke(AssociatedObject, new object[]
+            {
+                AssociatedObject,
+                EventArgs.Empty
+            });
+            typeof(AutoCompleteBox).GetMethod("OpeningDropDown", BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.Invoke(AssociatedObject, new object[]
+            {
+                false
+            });
 
             if (!AssociatedObject.IsDropDownOpen)
             {
-                //We *must* set the field and not the property as we need to avoid the changed event being raised (which prevents the dropdown opening).
                 var ipc = typeof(AutoCompleteBox).GetField("_ignorePropertyChange", BindingFlags.NonPublic | BindingFlags.Instance);
 
                 if (ipc?.GetValue(AssociatedObject) is bool ipcValue && !ipcValue)
@@ -76,5 +107,34 @@ internal class AutoCompleteBehavior : Behavior<AutoCompleteBox>
                 AssociatedObject.SetCurrentValue<bool>(AutoCompleteBox.IsDropDownOpenProperty, true);
             }
         }
+    }
+
+    private bool ShouldShowDropdown(object? sender)
+    {
+        if (AssociatedObject == null || _viewField == null)
+            return true;
+
+        if (sender is AutoCompleteBox autoCompleteBox && string.IsNullOrEmpty(autoCompleteBox.Text) && autoCompleteBox.ItemsSource != null && autoCompleteBox.ItemsSource.Cast<object>().Any())
+        {
+            return true;
+        }
+
+        // 获取_view字段的值
+        var view = _viewField.GetValue(AssociatedObject) as AvaloniaList<object>;
+
+        // 检查过滤后的项目数量
+        if (view == null)
+            return true;
+
+        // 判断是否显示下拉框：
+        // - 如果过滤后没有项目，不显示
+        // - 如果只有一个项目且该项目为空，不显示
+        if (view.Count == 0 || (view.Count == 1 && view[0] == null))
+        {
+            return false;
+        }
+
+
+        return true;
     }
 }
