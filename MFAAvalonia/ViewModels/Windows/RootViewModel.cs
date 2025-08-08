e524ca64378f09c61fdb4e49c468d113823f645b
SweetSmellFox <1254371679@qq.com>
2025-08-08 11:40:52 +0800

    chore: 更新至v1.6.3-beta.6

--- MFAAvalonia/ViewModels/Windows/RootViewModel.cs
+++ MFAAvalonia/ViewModels/Windows/RootViewModel.cs
@@ -36,7 +36,7 @@ public partial class RootViewModel : ViewModelBase
             // var minor = version.Minor >= 0 ? version.Minor : 0;
             // var patch = version.Build >= 0 ? version.Build : 0;
             // return $"v{SemVersion.Parse($"{major}.{minor}.{patch}")}";
-            return "v1.0"; // Hardcoded version for now, replace with dynamic versioning later
+            return "v1.6.3-beta.6"; // Hardcoded version for now, replace with dynamic versioning later
         }
     }
 
